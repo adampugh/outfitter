0b5c51d1cbbb3fa8f436e9fcc36f309a7a5e8fb9
Adam <adampugh@zoho.com>
2020-01-15 21:25:56 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./styles/index.scss";
 
 import Grid from './components/grid';
 import Navbar from './components/navbar';
-// import BlackDrip from './components/blackdrip';
 import Wishlist from './components/wishlist';
 import ComicsGrid from './components/comicsGrid';
 import ComicsWishlist from './components/comicWishlist';
