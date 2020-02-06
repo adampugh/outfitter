525d1d9877f8668f39ef9ae921ab3d35fa4a0375
Adam <adampugh@zoho.com>
2020-02-06 21:40:13 +0000

    Added styles

--- src/components/comicWishlist.js
+++ src/components/comicWishlist.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 import WishlistItem from './wishlistItem';
 
-import Spawn1 from '../images/comics/spawn1.jpg';
 import Spawn285 from '../images/comics/spawn285.jpg';
 import BatmanWhoLaughs2 from '../images/comics/batmanwholaughs2.jpg';
 import DarkNightsMetal2 from '../images/comics/darknightsmetal2.jpg';
 import AmazingSpiderman252 from '../images/comics/amazingspiderman252.jpg';
 
 const wishlistItems = [
-    { title: 'Spawn #1', img: Spawn1 },
     { title: 'Spawn #285', img: Spawn285 },
     { title: 'Batman Who Laughs #2 Dell\'Otto Virgin', img: BatmanWhoLaughs2 },
     { title: 'Dark Nights Metal #2 Mattina', img: DarkNightsMetal2 },
